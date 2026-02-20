b7f88395a6dfaa120561eaec8c15c1304fe36853
Ven4o <132142021+Ven4o@users.noreply.github.com>
2026-02-20 11:21:18 +0200

    Update Sorted.cs

--- Zadachaekip/Sorted.cs
+++ Zadachaekip/Sorted.cs
@@ -8,5 +8,23 @@ namespace Zadachaekip
 {
     internal class Sorted
     {
+        public static int[] SortArray(int[] array, bool ascending)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - i - 1; j++)
+        {
+            if ((ascending && array[j] > array[j + 1]) ||
+                (!ascending && array[j] < array[j + 1]))
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    return array;
+}
     }
 }
